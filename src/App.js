e5a9aa2750669825ff1b2a04e65593886ff99c5e
Oscar Urrutia <oscar.urrutia@amvsoluciones.com>
2023-03-10 08:35:37 +0100

    pwa workbox

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import { LanguageProvider } from "./lang/Provider/LanguageProvider";
 import router from "./router/router";
 import { RouterProvider } from "react-router-dom";
-
+import { WorkBoxProvider } from "react-workbox";
 
 function App() {
   return (
-    <Provider store={store}>
-      <QueryClientProvider client={new QueryClient()}>
-        <LanguageProvider>
-        <RouterProvider router={router} />
-        </LanguageProvider>
-      </QueryClientProvider>
-    </Provider>
+    <WorkBoxProvider interval={30 * 1000}>
+      <Provider store={store}>
+        <QueryClientProvider client={new QueryClient()}>
+          <LanguageProvider>
+            <RouterProvider router={router} />
+          </LanguageProvider>
+        </QueryClientProvider>
+      </Provider>
+    </WorkBoxProvider>
   );
 }
 
