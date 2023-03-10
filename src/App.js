8c42c39d5f6eee1d120fc6304c37fb72f7003d97
Oscar Urrutia <oscar.urrutia@amvsoluciones.com>
2023-03-10 08:48:38 +0100

    arreglo wb

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
+
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import { LanguageProvider } from "./lang/Provider/LanguageProvider";
 import router from "./router/router";
 import { RouterProvider } from "react-router-dom";
-import { WorkboxProvider } from "react-workbox";
+
+
 
 function App() {
   return (
-    <WorkboxProvider interval={30 * 1000}>
-      <Provider store={store}>
-        <QueryClientProvider client={new QueryClient()}>
-          <LanguageProvider>
-            <RouterProvider router={router} />
-          </LanguageProvider>
-        </QueryClientProvider>
-      </Provider>
-    </WorkboxProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={new QueryClient()}>
+        <LanguageProvider>
+        <RouterProvider router={router} />
+        </LanguageProvider>
+      </QueryClientProvider>
+    </Provider>
   );
 }
 
